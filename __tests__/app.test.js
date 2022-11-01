e3b5df763135bec96cda96fbb1d32c75da3d3d7c
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-01 17:12:19 -0600

    add horoscope to each sign, test and build /zodiac/:id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,17 @@ describe('zodiac routes', () => {
     });
     expect(res.body).toEqual(expected);
   });
+
+  it('/zodiac/:id should return sign detail', async () => {
+    const res = await request(app).get('/zodiac/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+      horoscope:
+        'Cosmic activity in the career area of your chart is encouraging you to do something out of the ordinary on the work front, something that people in positions of power can’t help but praise you for. Don’t do what has been done already, be a pioneer.',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
 });
