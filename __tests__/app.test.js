4d6624822a02d6239873624322b28abf27f48617
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-01 16:01:39 -0600

    change naming to better suit the data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,13 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { zodiacs } = require('../lib/zodiacs-data');
+const { signs } = require('../lib/zodiacs-data');
 
 describe('zodiac routes', () => {
-  it('/zodiacs should return a list of zodiacs', async () => {
+  it('/zodiac should return a list of zodiacs', async () => {
     const res = await request(app).get('/zodiac');
-    const expected = zodiacs.map((zodiac) => {
-      return { id: zodiac.id, name: zodiac.name };
+    const expected = signs.map((sign) => {
+      return { id: sign.id, name: sign.name };
     });
     expect(res.body).toEqual(expected);
   });
