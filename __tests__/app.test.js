cb97fc79e638bc1f61352148d67a1c311e94f7bb
GeoSauer <geoffrey.sauer89@gmail.com>
2022-11-01 19:06:13 -0600

    add horoscope array, test and build /horoscope/:name route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,4 +24,14 @@ describe('zodiac routes', () => {
     };
     expect(res.body).toEqual(aquarius);
   });
+
+  it('/horoscopes/:name should return a horoscope for that sign', async () => {
+    const res = await request(app).get('/horoscopes/aquarius');
+    const aquarius = {
+      name: 'aquarius',
+      horoscope:
+        'Cosmic activity in the career area of your chart is encouraging you to do something out of the ordinary on the work front, something that people in positions of power can’t help but praise you for. Don’t do what has been done already, be a pioneer.',
+    };
+    expect(res.body).toEqual(aquarius);
+  });
 });
